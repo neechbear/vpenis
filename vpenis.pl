66df7992d24161a2405dfe422c43ab8f20f369fe
John Barker <john@johnbarker.com>
2016-02-13 12:49:11 +0000

    Strict and warning compliant Run through Perltidy

--- vpenis.pl
+++ vpenis.pl
@@ -1,213 +1,240 @@
-#!/usr/bin/perl
+#!/usr/bin/perl -w
+use strict;
+use warnings;
 
 ###
 ###   vpenis.pl  -- perl-rewrite of   http://linuxfi.org/vpenis.sh
 ###                 original program understood only Linux, this has
 ###                 learned also about Solaris ...
 ###
-###
+###                 2016 https://github.com/neechbear/vpenis
 
-$uname_s = `uname -s`;  chomp($uname_s);
+my $in;
+my $MHz     = 0.0;
+my $disk_kB = 0;
+my $mem_kB  = 0;
+my $ok      = 0;
 
-$uptime_d = `uptime`; chomp($uptime_d);
+my $uname_s = `uname -s`;
+chomp($uname_s);
 
-$uptime_d =~ s/.* up ([0-9]+) day.*/\1/;
+my $uptime_d = `uptime`;
+chomp($uptime_d);
+if ( $uptime_d =~ /^\s+\S+\s+up\s+(\S+\s+\S+,\s+\S+),/ ) {
+    print $1;
+}
+else {
+    # If the system has been up for less than one day we could hit this
+    print "WARNING: Uptime format not recognised. Assuming 1 day\n";
+    $uptime_d = 0;
+}
+# printf "uname-s = '%s'   uptime = '%s'\n", $uname_s, $uptime_d;
 
-# printf "uname-s = '%s'   uptime = '%s'\n",$uname_s, $uptime_d;
+###############################################################################
+# BSD
 
-$ok = 0;
-
-if ($uname_s eq 'NetBSD' || $uname_s eq 'FreeBSD' || $uname_s eq 'OpenBSD' ||
-    $uname_s eq 'Darwin') {
+if (   $uname_s eq 'NetBSD'
+    || $uname_s eq 'FreeBSD'
+    || $uname_s eq 'OpenBSD'
+    || $uname_s eq 'Darwin' )
+{
     # NetBSD tested, others PRESUMED to work..
     # Darwin doesn't tell CPU MHzs in dmesg, but ...
 
     # Sum of CPU MHz values, no attention at how much work per MHz gets done..
-    $MHz = 0.0;
-    open(IN, 'dmesg|egrep \'^cpu.* MHz, \' |');
-    while (<IN>) {
-	chomp;
-	if (m/ ([0-9.]+) MHz/o) {
-	    $MHz += $1;
-	}
+    open( $in, '-|', 'dmesg|egrep \'^cpu.* MHz, \'' );
+    while (<$in>) {
+        chomp;
+        if (m/ ([0-9.]+) MHz/o) {
+            $MHz += $1;
+        }
     }
-    close IN;
+    close $in;
 
     # Darwin doesn't tell MHz numbers in dmesg..
-    $cpufmax = 0;
-    open(IN, 'sysctl -n hw.cpufrequency_max |');
-    while (<IN>) {
-	chomp;
-	$cpufmax = $_;
+    my $cpufmax = 0;
+    open( $in, '-|', 'sysctl -n hw.cpufrequency_max' );
+    while (<$in>) {
+        chomp;
+        $cpufmax = $_;
     }
-    close(IN);
+    close($in);
 
-    $cpuf = 0;
-    open(IN, 'sysctl -n hw.cpufrequency |');
-    while (<IN>) {
-	chomp;
-	$cpuf = $_;
+    my $cpuf = 0;
+    open( $in, '-|', 'sysctl -n hw.cpufrequency' );
+    while (<$in>) {
+        chomp;
+        $cpuf = $_;
     }
-    close(IN);
+    close($in);
 
-    $ncpu = 0;
-    open(IN, 'sysctl -n hw.ncpu |');
-    while (<IN>) {
-	chomp;
-	$ncpu = $_;
+    my $ncpu = 0;
+    open( $in, '-|', 'sysctl -n hw.ncpu' );
+    while (<$in>) {
+        chomp;
+        $ncpu = $_;
     }
-    close(IN);
+    close($in);
 
-    $ncpu = 1 if ($ncpu < 1);
-    $cpuf = $cpufmax if ($cpuf < $cpufmax);
+    $ncpu = 1        if ( $ncpu < 1 );
+    $cpuf = $cpufmax if ( $cpuf < $cpufmax );
 
-    if ($cpuf > 0 && $MHz < 1) {
-	$MHz = ($ncpu * $cpuf) / 1000000;
+    if ( $cpuf > 0 && $MHz < 1 ) {
+        $MHz = ( $ncpu * $cpuf ) / 1000000;
     }
 
-#printf "MHz: %ld\n",$MHz;
+    #printf "MHz: %ld\n",$MHz;
 
     # Note: Original shows USED memory, but it isn't so easily available
     #       in e.g. Solaris, so this is TOTAL memory
-    $mem_kB = 0;
-    open(IN, 'sysctl -n hw.physmem |');
-    while (<IN>) {
-	$mem_kB += ($_ / 1024.0);
+    open( $in, '-|', 'sysctl -n hw.physmem' );
+    while (<$in>) {
+        $mem_kB += ( $_ / 1024.0 );
     }
-    close(IN);
+    close($in);
 
-#printf "mem_kB: %ld\n",$mem_kB;
+    #printf "mem_kB: %ld\n",$mem_kB;
 
-    $disk_kB = 0;
-    open(IN, 'df -k -l |');
-    while (<IN>) {
-	chomp;
-	next if ($_ =~ m/^Filesyst/o);
-	if ($_ =~ m/^(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/o) {
-	    $dsk_dev = $1;
-	    $disk_kB += $2;
+    my $dsk_dev        = 0;
+    my $partition_size = 0;
+    open( $in, '-|', 'df -k -l' );
+    while (<$in>) {
+        chomp;
+        next if ( $_ =~ m/^Filesyst/o );
+        if ( $_ =~ m/^(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/o ) {
+            $dsk_dev        = $1;
+            $partition_size = $2;
+            $disk_kB += $partition_size;
 
-	    # Some disk systems are given extra credit...
-	    if ($dsk_dev =~ m!/dev/md!o) {
-		$disk_kB += $2;
-	    }
-	}
+            # Some disk systems are given extra credit...
+            if ( $dsk_dev =~ m!/dev/md!o ) {
+                $disk_kB += $partition_size;
+            }
+        }
     }
-    close IN;
+    close $in;
 
-#printf "disk_kB: %ld\n",$disk_kB;
+    #printf "disk_kB: %ld\n",$disk_kB;
 
     $ok = 1;
 }
 
+###############################################################################
+# SunOS
 
-if ($uname_s eq 'SunOS') {
+if ( $uname_s eq 'SunOS' ) {
+
     # Solaris understood, SunOS 4 not so...
 
     # Sum of CPU MHz values, no attention at how much work per MHz gets done..
-    $MHz = 0.0;
-    open(IN, 'psrinfo -v | grep MHz | awk \'{print $6}\' |');
-    while (<IN>) {
-	chomp;
-	$MHz += $_;
+    open( $in, '-|', 'psrinfo -v | grep MHz | awk \'{print $6}\'' );
+    while (<$in>) {
+        chomp;
+        $MHz += $_;
     }
-    close IN;
+    close $in;
 
-#printf "MHz: %ld\n",$MHz;
+    #printf "MHz: %ld\n",$MHz;
 
     # Note: Original shows USED memory, but it isn't so easily available
     #       in e.g. Solaris, so this is TOTAL memory
-    $mem_kB = 0;
-    open(IN, 'prtconf | egrep \'^Memory\' | awk \'{print $3}\' |');
-    while (<IN>) {
-	$mem_kB += (1024 * $_);
+    open( $in, '-|', 'prtconf | egrep \'^Memory\' | awk \'{print $3}\'' );
+    while (<$in>) {
+        $mem_kB += ( 1024 * $_ );
     }
-    close(IN);
+    close($in);
 
-#printf "mem_kB: %ld\n",$mem_kB;
+    #printf "mem_kB: %ld\n",$mem_kB;
 
-    $disk_kB = 0;
-    open(IN, 'df -k -l |');
-    while (<IN>) {
-	chomp;
-	next if ($_ =~ m/^Filesyst/o);
-	if ($_ =~ m/^(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/o) {
-	    $dsk_dev = $1;
-	    $disk_kB += $2;
+    open( $in, '-|', 'df -k -l' );
+    my $dsk_dev        = 0;
+    my $partition_size = 0;
+    while (<$in>) {
+        chomp;
+        next if ( $_ =~ m/^Filesyst/o );
+        if ( $_ =~ m/^(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/o ) {
+            $dsk_dev        = $1;
+            $partition_size = $2;
+            $disk_kB += $partition_size;
 
-	    # Some disk systems are given extra credit...
-	    if ($dsk_dev =~ m!/dev/md!o) {
-		$disk_kB += $2;
-	    }
-	}
+            # Some disk systems are given extra credit...
+            if ( $dsk_dev =~ m!/dev/md!o ) {
+                $disk_kB += $partition_size;
+            }
+        }
     }
-    close IN;
+    close $in;
 
-#printf "disk_kB: %ld\n",$disk_kB;
+    #printf "disk_kB: %ld\n",$disk_kB;
 
     $ok = 1;
 }
 
-if ($uname_s eq 'Linux') {
+###############################################################################
+# Linux
 
-    $MHz = 0;
-    open(IN, 'cat /proc/cpuinfo | grep \'^cpu MHz\' | awk \'{print $4;}\' |');
-    while (<IN>) {
-	chomp;
-	$MHz += $_;
+if ( $uname_s eq 'Linux' ) {
+
+    open( $in, '-|',
+        'cat /proc/cpuinfo | grep \'^cpu MHz\' | awk \'{print $4;}\'' );
+    while (<$in>) {
+        chomp;
+        $MHz += $_;
     }
-    close IN;
+    close $in;
 
-#printf "MHz: %ld\n",$MHz;
+    #printf "MHz: %ld\n",$MHz;
 
-    $mem_kB = 0.0;
-    open(IN, 'free | grep ^Mem | awk \'{print $2}\' |');
-    while (<IN>) {
-	$mem_kB += $_;
+    open( $in, '-|', 'free | grep ^Mem | awk \'{print $2}\'' );
+    while (<$in>) {
+        $mem_kB += $_;
     }
-    close IN;
+    close $in;
 
-#printf "mem_kB: %ld\n",$mem_kB;
+    #printf "mem_kB: %ld\n",$mem_kB;
 
     # Note: Original shows USED memory, but it isn't so easily available
     #       in e.g. Solaris, so this is TOTAL memory
 
-    $disk_kB = 0;
-    open(IN, 'df -k -l |');
-    while (<IN>) {
-	chomp;
-	next if ($_ =~ m/^Filesyst/o);
-	if ($_ =~ m/^(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/o) {
-	    $dsk_dev = $1;
-	    $disk_kB += $2;
+    my $dsk_dev        = 0;
+    my $partition_size = 0;
+    open( $in, '-|', 'df -k -l' );
+    while (<$in>) {
+        chomp;
+        next if ( $_ =~ m/^Filesyst/o );
+        if ( $_ =~ m/^(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)\s+(\S+)/o ) {
+            $dsk_dev        = $1;
+            $partition_size = $2;
+            $disk_kB += $partition_size;
 
-	    # Some disk systems are given extra credit...
-	    if ($dsk_dev =~ m!/dev/md!o) {
-		$disk_kB += $2;
-	    }
-	    if ($dsk_dev =~ m!/dev/sd!o) {
-		$disk_kB += $2;
-	    }
-	}
+            # Some disk systems are given extra credit...
+            if ( $dsk_dev =~ m!/dev/md!o ) {
+                $disk_kB += $partition_size;
+            }
+            if ( $dsk_dev =~ m!/dev/sd!o ) {
+                $disk_kB += $partition_size;
+            }
+        }
     }
-    close IN;
+    close $in;
 
-#printf "disk_kB: %ld\n",$disk_kB;
+    #printf "disk_kB: %ld\n",$disk_kB;
 
     $ok = 1;
 }
 
-if (!$ok) {
-    printf "SYSTEM NOT KNOWN:  uname_s = '%s'\n",$uname_s;
+if ( !$ok ) {
+    printf "SYSTEM NOT KNOWN:  uname_s = '%s'\n", $uname_s;
     exit 64;
 }
 
-$vpenis = 0.1 * (
-		 0.1 * $uptime_d +
-		 $MHz / 30.0 +
-		 $mem_kB / 1024.0 / 3.0 +
-		 $disk_kB / 1024.0 / 50.0 / 15.0 +
-		 70.0);
+my $vpenis =
+  0.1 *
+  ( 0.1 * $uptime_d +
+      $MHz / 30.0 +
+      $mem_kB / 1024.0 / 3.0 +
+      $disk_kB / 1024.0 / 50.0 / 15.0 +
+      70.0 );
 
 printf "%.1fcm\n", $vpenis;
 
@@ -223,4 +250,4 @@ exit 0;
 #    df -P -k -x nfs -x smbfs | grep -v '(1k|1024)-blocks' |
 #        awk '{if ($1 ~ "/dev/(scsi|sd)"){ s+= $2} s+= $2;} END {print s/1024/50"/15+70";}'
 #  ` | bc | sed 's/\(.$\)/.\1cm/'
-# 
+#
